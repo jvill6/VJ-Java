44fc415c8e8970feea2233385b10bbb4f75435a1
jvill6 <villj650@gmail.com>
2026-09-14 07:24:50 +0800

    added a super constructor call from details class

--- OOPRO_Java/src/Details.java
+++ OOPRO_Java/src/Details.java
@@ -6,7 +6,9 @@ public class Details extends Order {
     private String eventVenue;
     private int guestCount;
 
-    public Details(String eventDate, String eventVenue, int guestCount) {
+    public Details(Customer customerName, int orderId, int dishAmount, Menu menuPrice, int serviceCost, String eventDate, String eventVenue, int guestCount) {
+        super(customerName, orderId, dishAmount, menuPrice, serviceCost);
+
         this.eventDate = eventDate;
         this.eventVenue = eventVenue;
         this.guestCount = guestCount;
