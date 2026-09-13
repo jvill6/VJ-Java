ce47acd12acfce0f5c37254ee8bbbf1541515a8b
LovelyLegaspi <s2301022@usls.edu.ph>
2026-09-13 16:26:17 +0800

    idunno what to add

---
+++ OOPRO_Java/src/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    private int customerId
+    private String name
+    private String phone
+
+    Customer (int customerId, String name, String phone) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phone = phone;
+    }
+}
---
+++ OOPRO_Java/src/Menu.java
@@ -0,0 +1,12 @@
+public class Menu {
+    private String itemName
+    private double price
+    private String category
+
+    Menu (String itemName, double price, String category) {
+        this.itemName = itemName;
+        this.price = price;
+        this.category = category;
+    }
+
+}
