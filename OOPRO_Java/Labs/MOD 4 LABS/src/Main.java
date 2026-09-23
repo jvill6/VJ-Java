189852cba10fd648794eed9ecb887c415b6de9b5
jvill6 <villj650@gmail.com>
2026-09-23 11:48:22 +0800

    Addition of module 4 labs

---
+++ OOPRO_Java/Labs/MOD 4 LABS/src/Main.java
@@ -0,0 +1,11 @@
+class main {
+    void main() {
+
+        Circle circle = new Circle();
+        circle.draw();
+
+        Square square = new Square();
+        square.draw();
+
+    }
+}
---
+++ OOPRO_Java/Labs/MOD 4 LABS/src/Shape.java
@@ -0,0 +1,9 @@
+public class Shape {
+
+    void draw() {
+        System.out.println("Drawing a shape");
+    }
+
+}
+
+
